beef354654d4b9757ac435762ce6cf88ebee6a1d
hyemi1129 <kimhyemi1243@gmail.com>
2024-03-19 10:24:06 +0900

    03/19

---
+++ opencv13.py
@@ -0,0 +1,21 @@
+import os
+import cv2 as cv
+
+img = cv.imread('puppy.jpg')
+
+height = img.shape[0]
+width = img.shape[1]
+
+for y in range(0, height):
+    img.itemset(y, int(width/2), 0, 0)
+    img.itemset(y, int(width/2), 1, 0)
+    img.itemset(y, int(width/2), 2, 255)
+
+    for x in range(0, width):
+        img.itemset(int(height/2), x, 0, 255)
+        img.itemset(int(height/2), x, 1, 0)
+        img.itemset(int(height/2), x, 2, 0)
+
+cv.imshow('reslut', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
